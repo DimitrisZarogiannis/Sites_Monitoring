cf742c2b66fa057ed392d7190b915c034e7171d5
Dimitris Zarogiannis <dimitriszarogiannis461@gmail.com>
2019-06-03 14:11:09 +0300

    Added Metric Histograms func

--- statistical_analysis_1.py
+++ statistical_analysis_1.py
@@ -1,5 +1,7 @@
 # Import packages
 from pymongo import MongoClient
+import matplotlib.pyplot as plt
+import numpy as np
 import re
 
 # Simple statistical analysis class for the following metrics:
@@ -12,51 +14,58 @@ import re
 class StatisticalAnalysis:
     client = MongoClient('mongodb://localhost:27017')
     db = client['Articles_DB']
-    articles_conn = db.articles11
+    articles_conn = list()
     articles_count = 0
     term_freq = {}
-    total_title_characters = 0
-    total_title_words = 0
-    total_article_chars = 0
-    total_article_words = 0
-    average_title_characters = int()
-    average_title_words = int()
-    average_article_chars = int()
-    average_article_words = int()
+    metrics_data = list()
+
+    # Find all article collections in the MongoDB
+    def find_all_article_collections(self):
+        collections = self.db.list_collection_names()
+        if 'urls' in collections:
+            collections.remove('urls')
+        self.articles_conn = collections
 
     # Calculate the desired article metrics
     def calculate_article_metrics(self):
-        articles = list(self.articles_conn.find())
-        self.articles_count = len(articles)
+        for collection in self.articles_conn:
+            articles = list(self.db.get_collection(collection).find())
+            self.articles_count = len(articles)
 
-        for article in articles:
+            total_title_characters = int()
+            total_title_words = int()
+            total_article_chars = int()
+            total_article_words = int()
 
-            article_title = article['title']
-            article_body = article['article']
+            for article in articles:
 
-            # Calculate number of characters/words per article title
-            title_chars_number = len(str(article_title).replace(' ', ''))
-            title_words_list = str(article_title).split(' ')
-            title_words_number = len(title_words_list)
-            self.total_title_characters += title_chars_number
-            self.total_title_words += title_words_number
+                article_title = article['title']
+                article_body = article['article']
 
-            # Calculate article number of characters/words
-            article_chars_number = len(str(article_body).replace(' ', ''))
-            article_words_list = article_body.split(' ')
-            article_words_number = len(article_words_list)
-            self.total_article_chars += article_chars_number
-            self.total_article_words += article_words_number
+                # Calculate number of characters/words per article title
+                title_chars_number = len(str(article_title).replace(' ', ''))
+                title_words_list = str(article_title).split(' ')
+                title_words_number = len(title_words_list)
+                total_title_characters += title_chars_number
+                total_title_words += title_words_number
 
-        # Get average metrics for the whole blog collection
-        self.average_title_characters = int(self.total_title_characters / self.articles_count)
-        self.average_title_words = int(self.total_title_words / self.articles_count)
-        self.average_article_chars = int(self.total_article_chars / self.articles_count)
-        self.average_article_words = int(self.total_article_words / self.articles_count)
-        print(self.average_title_characters)
-        print(self.average_title_words)
-        print(self.average_article_chars)
-        print(self.average_article_words)
+                # Calculate article number of characters/words
+                article_chars_number = len(str(article_body).replace(' ', ''))
+                article_words_list = article_body.split(' ')
+                article_words_number = len(article_words_list)
+                total_article_chars += article_chars_number
+                total_article_words += article_words_number
+
+            # Get average metrics for the whole blog collection
+            average_title_characters = int(total_title_characters / self.articles_count)
+            average_title_words = int(total_title_words / self.articles_count)
+            average_article_chars = int(total_article_chars / self.articles_count)
+            average_article_words = int(total_article_words / self.articles_count)
+
+            stats = [average_title_characters, average_title_words,
+                     average_article_chars, average_article_words]
+
+            self.metrics_data.append(stats)
 
     # Calculate number of articles per day
     def analyse_site_activity(self):
@@ -94,6 +103,7 @@ class StatisticalAnalysis:
 
         print(self.term_freq)
 
+    # Fill the term freq dictionary function
     def populate_term_freq_dict(self, wordslist, termslist):
         for word in wordslist:
             if word in termslist:
@@ -102,9 +112,58 @@ class StatisticalAnalysis:
                 except:
                     self.term_freq.update({word: 1})
 
+    # Create metrics histograms
+    def create_histograms(self):
+        fig = plt.figure()
+        ax1 = fig.add_subplot(2, 1, 1)
+        ax2 = fig.add_subplot(2, 1, 2)
+        fig2 = plt.figure()
+        ax3 = fig2.add_subplot(2, 1, 1)
+        ax4 = fig2.add_subplot(2, 1, 2)
+
+        Average_title_chars = list()
+        for listitem in self.metrics_data:
+            Average_title_chars.append(listitem[0])
+        Average_title_chars = np.asarray(Average_title_chars)
+        n, bins, patches = ax1.hist(Average_title_chars, histtype='bar', label=['Characters'])
+        ax1.set_ylabel('Frequency')
+        ax1.set_xlabel('Average article title chars distribution')
+        ax1.legend(loc="upper right")
+
+        Average_title_words = list()
+        for listitem in self.metrics_data:
+            Average_title_words.append(listitem[1])
+        Average_title_words = np.asarray(Average_title_words)
+        n, bins, patches = ax2.hist(Average_title_words, histtype='bar', label=['Words'])
+        ax2.set_ylabel('Frequency')
+        ax2.set_xlabel('Average article title words distribution')
+        ax2.legend(loc="upper right")
+
+        Average_article_chars = list()
+        for listitem in self.metrics_data:
+            Average_article_chars.append(listitem[2])
+        Average_article_chars = np.asarray(Average_article_chars)
+        n, bins, patches = ax3.hist(Average_article_chars, histtype='bar', label=['Characters'])
+        ax3.set_ylabel('Frequency')
+        ax3.set_xlabel('Average article chars distribution')
+        ax3.legend(loc="upper right")
+
+        Average_article_words = list()
+        for listitem in self.metrics_data:
+            Average_article_words.append(listitem[3])
+        Average_article_words = np.asarray(Average_article_words)
+        n, bins, patches = ax4.hist(Average_article_words, histtype='bar', label=['Words'])
+        ax4.set_ylabel('Frequency')
+        ax4.set_xlabel('Average article words distribution')
+        ax4.legend(loc="upper right")
+
+        plt.show()
+
 
 if __name__ == "__main__":
     stat = StatisticalAnalysis()
+    stat.find_all_article_collections()
     stat.calculate_article_metrics()
-    stat.analyse_site_activity()
-    stat.calculate_genres_frequency()
+    stat.create_histograms()
+    # stat.analyse_site_activity()
+    # stat.calculate_genres_frequency()
